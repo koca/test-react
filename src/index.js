d65849baeaeff77fae8846e0d09e55a9269ea47b
Mesut <imesutkoca@gmail.com>
2019-11-11 21:44:16 +0300

    add counter demo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Counter from "./Counter";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Counter />
     </div>
   );
 }
